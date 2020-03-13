944058a4bca3080fcb49074586a4321a02c32fd9
jpham35 <49501108+jpham35@users.noreply.github.com>
2020-03-13 15:00:47 -0500

    updates for codepath lab 3

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.flashcard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        String s = "";
+        for(Flashcard flashcard: allFlashcards) {
+            s += "QUESTION: " + flashcard.getQuestion() +
+                    " ANSWER: " + flashcard.getAnswer() + " " + flashcard.getWrongAnswer1() +
+                    " " + flashcard.getWrongAnswer2() + "\n";
+        }
+        Log.d("MAINACTIVITY", s);
 
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener()
         {
@@ -34,6 +47,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
+
         findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -41,6 +59,25 @@ public class MainActivity extends AppCompatActivity {
             {
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+            }
+        });
+
+        findViewById(R.id.next_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                    currentCardDisplayedIndex = 0;
+                }
+
+                // set the question and answer TextViews with data from the database
+                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.
+                        get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.
+                        get(currentCardDisplayedIndex).getAnswer());
             }
         });
     }
@@ -57,9 +94,15 @@ public class MainActivity extends AppCompatActivity {
                 TextView currentFlashcardAnswer = findViewById(R.id.flashcard_answer);
                 currentFlashcardAnswer.setText(newAnswer);
                 currentFlashcardQuestion.setText(newQuestion);
+
+                flashcardDatabase.insertCard(new Flashcard(newQuestion, newAnswer));
+                allFlashcards = flashcardDatabase.getAllCards();
             }
         }
 
     }
 
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayedIndex = 0;
 }
